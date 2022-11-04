df0b52f9f98bf99bbe77e7c12f9cdfb4176c04e4
tboury <theo.boury@ens-lyon.fr>
2022-11-04 15:39:50 -0400

    First synthetic example generated to be tested

--- TestFuzzTree.py
+++ TestFuzzTree.py
@@ -74,15 +74,61 @@ def test_graph_where_pattern_is_detected(GPpath = "rin163.pickle", GTlistfolder 
     return resu
 
 
+#name="1Y27-1B53misslabeledCWW1edgesmissing1falselabelCHSintoCWH.nxpickle"
+#with open("1Y27.nxpickle",'rb') as fG:
+    #GG = pickle.load(fG)
+    #print([(i,j) for (i,j,t) in GG.edges.data() if i == ('X', 24) or j == ('X', 20)])
+    #GG.remove_edge(('X', 53), ('X', 54))
 
-import_rin(163)
+    #GG.remove_edge(('X', 48), ('X', 52))
+    #GG.remove_edge(('X', 52), ('X', 53))
+    #GG.remove_edge(('X', 24), ('X', 20))
+    #print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 50) and j == ('X', 49)])
+    #GG.remove_edge(('X', 52), ('X', 53))
+    #GG.add_edge(('X', 52), ('X', 53), label='CWW', near=False, long_range=False)
+    #print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 52) and j == ('X', 53)])
+    #print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 50) and j == ('X', 49)])
+    #GG.remove_edge(('X', 50), ('X', 49))
+    #GG.add_edge(('X', 50), ('X', 49), label='CWH', near=False, long_range=False)
+    #print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 50) and j == ('X', 49)])
+#with open(name, 'wb') as ff:
+#    pickle.dump(GG, ff)
+
+name="1Y27-4falselabelsCHSintoCWHandTHWintoTHSandTHWintoTHSandcWWintoTHS.nxpickle"
+with open("1Y27.nxpickle",'rb') as fG:
+    GG = pickle.load(fG)
+
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 50) and j == ('X', 49)])
+    GG.remove_edge(('X', 50), ('X', 49))
+    GG.add_edge(('X', 50), ('X', 49), label='CWH', near=False, long_range=False)
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 50) and j == ('X', 49)])
+
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 53) and j == ('X', 20)])
+    GG.remove_edge(('X', 53), ('X', 20))
+    GG.add_edge(('X', 53), ('X', 20), label='THS', near=False, long_range=False)
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 53) and j == ('X', 20)])
+
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 20) and j == ('X', 53)])
+    GG.remove_edge(('X', 20), ('X', 53))
+    GG.add_edge(('X', 20), ('X', 53), label='THS', near=False, long_range=False)
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 20) and j == ('X', 53)])
+
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 46) and j == ('X', 54)])
+    GG.remove_edge(('X', 46), ('X', 54))
+    GG.add_edge(('X', 46), ('X', 54), label='THS', near=False, long_range=False)
+    print([(i,j, t['label']) for (i,j,t) in GG.edges.data() if i == ('X', 46) and j == ('X', 54)])
+with open(name, 'wb') as ff:
+    pickle.dump(GG, ff)
+
+
+#import_rin(163)
 
 #first_test_mapping()
 
 #first_test_varna()
 
 
-test_graph_where_pattern_is_detected(GPpath = "rin163.pickle", GTlistfolder = "RNAstorage", BooleanListGPinGT = [0,0,0,0,0,0,0,0,0,0], nb_samples=100)
+#test_graph_where_pattern_is_detected(GPpath = "rin163.pickle", GTlistfolder = "RNAstorage", BooleanListGPinGT = [0,0,0,0,0,0,0,0,0,0], nb_samples=100)
 
 
 #([(1, ('X', 52)), (2, ('X', 46)), (3, ('X', 47)), (4, ('X', 48)), (5, ('X', 49)), (6, ('X', 50)), (7, ('X', 51)), (8, ('X', 20)), (9, ('X', 53)), (10, ('X', 22)), (11, ('X', 24)), (12, ('X', 25)), (13, ('X', 21)), (14, ('X', 54))], 14)
